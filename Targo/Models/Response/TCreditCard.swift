bfe3754a917c257708050c728fdaea83df7f16e1
Vladimir Kokhanevich <vladimir.kokhanevich@gmail.com>
2016-08-11 10:24:11 +0300

    Working on payments, minor improvements

---
+++ Targo/Models/Response/TCreditCard.swift
@@ -0,0 +1,13 @@
+//
+//  TCreditCard.swift
+//  Targo
+//
+//  Created by Vladimir Kokhanevich on 06/08/16.
+//  Copyright © 2016 Targo. All rights reserved.
+//
+
+import UIKit
+
+class TCreditCard: NSObject {
+
+}
---
+++ Targo/TNSURLProtocolHelper.swift
@@ -0,0 +1,13 @@
+//
+//  TNSURLProtocolHelper.swift
+//  Targo
+//
+//  Created by Vladimir Kokhanevich on 10/08/16.
+//  Copyright © 2016 Targo. All rights reserved.
+//
+
+import UIKit
+
+class TNSURLProtocolHelper: NSURLProtocol {
+
+}
